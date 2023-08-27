259dc971083a9165472e0eeb1a4c215a91a3abe8
O-ho <pazzang0@gmail.com>
2023-08-27 22:54:44 +0900

    feat: my-draft page

---
+++ src/app/myDraft/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { headers } from "next/headers";
+import { PostProps } from "@/app/drafts/page";
+import Post from "@/app/components/Post";
+
+async function getDrafts() {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/myDraft`, {
+    method: "GET",
+    headers: headers(),
+  });
+  const drafts = await res.json();
+
+  return { post: drafts };
+}
+async function MyDraftPage() {
+  const { post }: { post: PostProps[] } = await getDrafts();
+
+  return (
+    <div className={"flex px-8"}>
+      <div className="page">
+        <h1 className={"font-cafe24 tracking-widest text-3xl"}>MY POSTINGS</h1>
+        <main>
+          {post.map((item: PostProps) => (
+            <div key={item.id} className="post">
+              <Post post={item} />
+            </div>
+          ))}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default MyDraftPage;
