6413678acc8064cd6e18bb3ac05b76e792835eee
O-ho <pazzang0@gmail.com>
2023-08-27 22:54:49 +0900

    feat: my-draft route

---
+++ src/app/api/myDraft/route.ts
@@ -0,0 +1,27 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
+import { NextResponse } from "next/server";
+import { getSession } from "next-auth/react";
+import prisma from "@/app/lib/prisma";
+
+export async function GET(req: any, res: any) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ status: "fail", message: "You are not logged in" }),
+      { status: 401 },
+    );
+  }
+  const result = await prisma.post.findMany({
+    where: {
+      author: { email: session?.user?.email },
+      published: false,
+    },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return NextResponse.json(result);
+}
