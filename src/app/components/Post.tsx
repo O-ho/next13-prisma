41cc4c2d8605c59c0605c017a573a8aed3e45b9d
O-ho <pazzang0@gmail.com>
2023-08-27 22:55:32 +0900

    mod: style

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -19,14 +19,13 @@ const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   return (
     <div
       className={
-        "mt-8 border-slate-200 border-4 rounded-2xl p-4 shadow-lg shadow-gray-200"
+        "w-fit mt-8 border-slate-200 border-4 rounded-2xl p-4 shadow-lg shadow-gray-200"
       }
     >
       <h2 className={"font-cafe24 tracking-widest text-3xl"}>{post.title}</h2>
       <p className={"font-pretendard text-lg my-4 italic"}>By - {authorName}</p>
       {/* eslint-disable-next-line react/no-children-prop */}
       <span className={"font-pretendard text-2xl tracking-wide leading-10"}>
-        {" "}
         {post.content}
       </span>
     </div>
