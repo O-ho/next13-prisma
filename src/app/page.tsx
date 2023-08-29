f1333f0381e2cf801f8359873886b8314a9682fe
blockodyssey-yhkim <yhkim@blockodyssey.io>
2023-08-29 17:05:05 +0900

    carousel setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import ListUser from "@/app/components/ListUser";
+import HomeCarousel from "@/app/components/Carousel/HomeCarousel";
 
 export default function Home() {
   return (
     <main>
       <div>활성화 유저</div>
       <ListUser />
+      <HomeCarousel />
     </main>
   );
 }
