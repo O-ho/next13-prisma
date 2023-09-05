7f0e25ffccaa74026a44a6bd02d6bdd6e476f336
O-ho <pazzang0@gmail.com>
2023-09-06 08:46:48 +0900

    mod: CSR issue modify

--- src/app/api/myDraft/route.ts
+++ src/app/api/myDraft/route.ts
@@ -1,7 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/lib/auth";
 import { NextResponse } from "next/server";
-import { getSession } from "next-auth/react";
 import prisma from "@/app/lib/prisma";
 
 export async function GET(req: any, res: any) {
